d91657b7317587ba3e9c9682a761740e0480f949
Karin Kikas <karin.kikas@gmail.com>
2023-06-25 18:14:59 +0300

    Use GitHub Actions

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/lumav_FE_test_02_05_2022',
-  experimental: {
-    appDir: true,
-  },
 }
 
 module.exports = nextConfig
